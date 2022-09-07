fc480cd8d5efaf5d9d92754f454be49d9e5238a8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-07 08:17:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,20 @@ import streamlit as st
 
 st.header('Original code')
 
-first_val = st.number_input(“Enter number:”)
-second_val = st.number_input(“Enter second number”)
+first_val = st.number_input('Enter number:')
+second_val = st.number_input('Enter second number')
 add_val = first_val + second_val
 st.write(add_val)
 
 
 st.header('Revised code')
 
-with st.form("my_form"):
-  first_val = st.number_input(“Enter number:”)
-  second_val = st.number_input(“Enter second number”)
+with st.form('my_form'):
+  first_value = st.number_input('Enter number:')
+  second_value = st.number_input('Enter second number')
 
-  submitted = st.form_submit_button("Submit")
+  submitted = st.form_submit_button('Submit')
   if submitted:
-      add_val = first_val + second_val
+      add_value = first_value + second_value
 
-st.write(add_val)
+st.write(add_value)
